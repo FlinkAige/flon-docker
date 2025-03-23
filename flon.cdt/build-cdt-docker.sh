6f766effcc7034884f4f64be9c7b884c25741998
Joss <281900360@qq.com>
2025-03-23 11:44:32 +0800

    update

--- flon.cdt/build-cdt-docker.sh
+++ flon.cdt/build-cdt-docker.sh
@@ -1,1 +1,5 @@
-docker build -t build-flon-deb:1.0.1 . --no-cache
+source $1
+# Default values for build parameters
+VERSION=${VERSION:-"0.5.0-alpha"}
+
+docker build -t build-flon-deb:${VERSION} . --no-cache
