6207640bb3d2cd4724307347a786ab73556412f9
Joss <281900360@qq.com>
2025-03-23 19:51:48 +0800

    update

--- flon.scan/docker-run/run.sh
+++ flon.scan/docker-run/run.sh
@@ -4,4 +4,7 @@ export $(grep -v '^#' $1 | xargs)
 
 envsubst < ./env > ./env.processed
 
+
 docker-compose --env-file $1 --env-file ./env.processed up -d
+source ./env.processed
+sudo iptables -I INPUT -p tcp -m tcp --dport "${POSTGRES_PORT}" -j ACCEPT
