77d30ce1c06504f3daeeae147bb2cae443399912
Joss <281900360@qq.com>
2025-03-24 16:59:49 +0800

    update

--- flon.chain/node-wal/run-fuwal.sh
+++ flon.chain/node-wal/run-fuwal.sh
@@ -13,7 +13,7 @@ mkdir -p $NOD_DIR/bin $NOD_DIR/conf $NOD_DIR/data $NOD_DIR/logs $NOD_DIR/bin-scr
 cp ./bin/run-wallet.sh $NOD_DIR/bin/
 cp ./bin/.bashrc $NOD_DIR/bin/
 cp ./config.ini $NOD_DIR/conf/
-cp ./bin-script/* $NOD_DIR/bin-script/
+cp -r ./bin-script/ $NOD_DIR/
 chmod +x $NOD_DIR/bin/run-wallet.sh
 
 docker-compose up -d
