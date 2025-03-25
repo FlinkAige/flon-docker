d2f777544f50705d2fe505b1f5076d4cf6b2e698
Joss <281900360@qq.com>
2025-03-25 15:23:43 +0800

    update

--- flon.chain/node-wal/bin-script/treg.sh
+++ flon.chain/node-wal/bin-script/treg.sh
@@ -1,4 +1,4 @@
 creator=$1
 acct=$2
 pubkey=$3
-fucli -u $turl system newaccount $creator $acct $pubkey --transfer-quant "5.00000000 FLON" -p $creator
+fucli -u $turl system newaccount $creator $acct $pubkey --fund-account "5.00000000 FLON" -p $creator
