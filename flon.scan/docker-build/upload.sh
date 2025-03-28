17440ab9b168422dc8fce6a3c263c04cac8f5f50
Joss <281900360@qq.com>
2025-03-28 19:47:38 +0800

    update

---
+++ flon.scan/docker-build/upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -f ~/flon.env ]; then
+  source ~/flon.env
+else 
+  echo "Error: ~/flon.env file not found!"
+  exit 1
+fi
+IMAGE_NAME="fullon/history-tools"
+
+bash -x ../../commtool/docker_upload.sh $GITHUB_USERNAME $IMAGE_NAME $VERSION
