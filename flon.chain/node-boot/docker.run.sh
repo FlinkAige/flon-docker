4150f8f79f44cdeb37587f4531fde8412a047e62
Joss <281900360@qq.com>
2025-03-29 20:16:39 +0800

    updat

--- flon.chain/node-boot/docker.run.sh
+++ flon.chain/node-boot/docker.run.sh
@@ -1,2 +1,8 @@
+# 检查参数
+if [ $# -eq 0 ]; then
+    echo "错误：缺少初始化参数 节点地址" 
+    exit 1
+fi
+
 docker cp ./devnet flon_wal:/root/
 docker exec -it flon_wal bash -c "cd ~/devnet && ./run.init.chain.sh \"$1\""
