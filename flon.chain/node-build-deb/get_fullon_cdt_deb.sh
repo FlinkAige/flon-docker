0501d423b254b348d91b43d3d9bacdf5d788fb93
Joss <281900360@qq.com>
2025-04-10 22:55:51 +0800

    update

--- flon.chain/node-build-deb/get_fullon_cdt_deb.sh
+++ flon.chain/node-build-deb/get_fullon_cdt_deb.sh
@@ -4,6 +4,6 @@ if [ -f ~/flon.env ]; then
     source ~/flon.env
 fi
 IMG=${NODE_IMG_HEADER}floncore/floncdt:${CDT_VERSION}
-package_name="flon.cdt"
+package_name="flon-core"
 
 bash -x ./get.deb.package.sh $IMG $package_name
