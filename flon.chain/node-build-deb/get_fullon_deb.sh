1e13dcc30c6b0c74b5a9bd7a9a1741ffc64963b3
Joss <281900360@qq.com>
2025-04-10 21:40:52 +0800

    update

--- flon.chain/node-build-deb/get_fullon_deb.sh
+++ flon.chain/node-build-deb/get_fullon_deb.sh
@@ -2,6 +2,6 @@
 if [ -f ~/flon.env ]; then
     source ~/flon.env
 fi
-IMG=${NODE_IMG_HEADER}fullon/funod:${FULLON_VERSION}
+IMG=${NODE_IMG_HEADER}floncore/funod:${FULLON_VERSION}
 package_name="fullon"
 bash -x ./get.deb.package.sh $IMG $package_name
